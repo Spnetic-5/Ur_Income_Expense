14b20469afa9ef13c2ab42b8e9868a311f69f120
Saurabh Suresh Powar <66636289+Spnetic-5@users.noreply.github.com>
2021-02-14 16:23:39 +0530

    Updated

--- expenseswebsite/expenses/migrations/urls.py
+++ expenseswebsite/expenses/migrations/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-path('',views.index, name='expenses'),
+    path('',views.index, name='expenses'),
+    path('add-expenses',views.add_expense, name='add-expenses')
 ]
--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'expenses/index.html')    
+    return render(request, 'expenses/index.html') 
+
+def add_expense(request):
+    return render(request, 'expenses/add_expense.html')
